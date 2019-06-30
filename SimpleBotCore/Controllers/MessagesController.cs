54fd7bdb76cb2dd268f1eb2f3fc14463ee0345a9
Moraes <gmmoraesbr@gmail.com>
2019-06-30 18:59:49 -0300

    Subir projeto com crud do mongo pronto.

---
+++ SimpleBotCore/Controllers/MessagesController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Schema;
+using SimpleBotCore.Logic;
+using SimpleBotCore.Moldes;
+using SimpleBotCore.Services;
+
+namespace SimpleBotCore.Controllers
+{
+    [Route("api/[controller]")]
+    public class MessagesController : Controller
+    {        
+        private readonly UsuarioService _usuarioService;
+        public static int contador = 1;
+        public MessagesController(UsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World";
+        }
+
+        // POST api/messages
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Activity activity)
+        {
+            if (activity != null && activity.Type == ActivityTypes.Message)
+            {
+                await HandleActivityAsync(activity);
+            }
+
+            // HTTP 202
+            return Accepted();
+        }
+
+        // Estabelece comunicacao entre o usuario e o SimpleBotUser
+        async Task HandleActivityAsync(Activity activity)
+        {
+            string text = activity.Text;
+            string userFromId = activity.From.Id;
+            string userFromName = activity.From.Name;
+            
+            Usuario usuario = new Usuario {
+                Username = userFromName,
+                Text = text
+            };
+
+            string response = _usuarioService.Create(usuario, contador);
+            contador++;
+            await ReplyUserAsync(activity, response);
+        }
+
+        // Responde mensagens usando o Bot Framework Connector
+        async Task ReplyUserAsync(Activity message, string text)
+        {
+            var connector = new ConnectorClient(new Uri(message.ServiceUrl));
+            var reply = message.CreateReply(text);
+
+            await connector.Conversations.ReplyToActivityAsync(reply);
+        }
+    }
+}
---
+++ SimpleBotCore/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using SimpleBotCore.Logic;
+using SimpleBotCore.Moldes;
+using SimpleBotCore.Services;
+
+namespace SimpleBotCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<UsuarioDatabaseSettings>(
+            Configuration.GetSection(nameof(UsuarioDatabaseSettings)));
+
+            services.AddSingleton<IUsuariostoreDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<UsuarioDatabaseSettings>>().Value);
+
+            services.AddSingleton<UsuarioService>();
+            //services.AddSingleton<SimpleBotUser>();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
